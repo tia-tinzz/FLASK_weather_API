1df1be59a6def6c9ec57a6b7fa8c8f4204207515
new <tinithomas27@gmail.com>
2022-08-23 18:38:34 +0530

    first commit

---
+++ weather.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+
+# import json to load JSON data to a python dictionary
+import json
+
+# urllib.request to make a request to api
+import urllib.request
+
+
+app = Flask(__name__)
+
+@app.route('/', methods =['POST', 'GET'])
+def weather():
+	if request.method == 'POST':
+		city = request.form['city']
+	else:
+		# for default name mathura
+		city = 'mathura'
+
+	# your API key will come here
+	api = 'c3fe1c2bdbc5ea1c25627aff6ce51afa'
+
+	# source contain json data from api
+	source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+api).read()
+	# converting JSON data to a dictionary
+	list_of_data = json.loads(source)
+
+	# data for variable list_of_data
+	data = {
+		"country_code": str(list_of_data['sys']['country']),
+		"coordinate": str(list_of_data['coord']['lon']) + ' '
+					+ str(list_of_data['coord']['lat']),
+		"temp": str(list_of_data['main']['temp']) + 'k',
+		"pressure": str(list_of_data['main']['pressure']),
+		"humidity": str(list_of_data['main']['humidity']),
+	}
+	print(data)
+	return render_template('index.html', data = data)
+
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
